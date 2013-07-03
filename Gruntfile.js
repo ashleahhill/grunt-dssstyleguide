28b5ef96a86cec4f2d6350298266147f3ec4ac11
ashleahhill <ashleahhill@gmail.com>
2013-07-02 20:31:04 -0400

    configurable now #2

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,92 +27,26 @@ module.exports = function(grunt) {
 
 		// Before generating any new files, remove any previously-created files.
 		clean: {
-			tests: ['tmp','tmp2'],
-		},
-
-		// dss config
-		dss: {
-			docs: {
-				options: {
-					template: 'test/fixtures/template/',
-					parsers: {
-						// Describe parsing a state
-						state: function(i, line, block){
-							var state = line.split(' - ');
-							return {
-								name: (state[0]) ? dss.trim(state[0]) : '',
-								escaped: (state[0]) ? dss.trim(state[0].replace('.', ' ').replace(':', ' is-')) : '',
-								description: (state[1]) ? dss.trim(state[1]) : ''
-							};
-						},
-						// Finds @link in comment blocks
-						link: function(i, line, block){
-							// Replace link with HTML wrapped version
-							var exp = /(b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-							line.replace(exp, "<a href='$1'>$1</a>");
-							return line;
-						}
-					}
-				},
-				files: {
-					'tmp2/':['test/fixtures/*.{css,scss,sass,less,styl}']
-				}
-			}
-		},
-
-		// compass config
-		compass: {
-			options: {
-				//config is relative to this file, not config file
-				config: 'test/config.rb'
-			},
-			dss:{
-				options:{
-					sassDir: 'test/fixtures/template/assets/source',
-					cssDir:  'test/fixtures/template/assets/css'
-				}
-			},
-			test:{}
+			tests: ['tmp','tmp2','tmp3']
 		},
 
 		// Configuration to be run (and then tested).
 		styleguide: {
-			default_options: {
+			dev: {},
+			dist: {
 				options: {
-				},
-				files: {
-					'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-				},
-			},
-			custom_options: {
-				options: {
-					separator: ': ',
-					punctuation: ' !!!',
-				},
-				files: {
-					'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-				},
-			},
-		},
-
-		// Configuration to be run (and then tested).
-		styleguide_template: {
-			default_options: {
-				options: {
-				},
-				files: {
-					'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-				},
-			},
-			custom_options: {
-				options: {
-					separator: ': ',
-					punctuation: ' !!!',
-				},
-				files: {
-					'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-				},
-			},
+					compass: {
+						outputStyle:'compact',
+						noLineComments:true,
+						force:true
+					},
+					dss: {
+						files: {
+							'tmp3/':['test/fixtures/*.{css,scss,sass,less,styl}']
+						}
+					}
+				}
+			}
 		},
 
 		// Unit tests.
@@ -126,8 +60,6 @@ module.exports = function(grunt) {
 	grunt.loadTasks('tasks');
 
 	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-dss');
-	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-jade');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-clean');
@@ -138,6 +70,8 @@ module.exports = function(grunt) {
 	grunt.registerTask('test', ['clean', 'styleguide', 'nodeunit']);
 
 	// By default, lint and run all tests.
-	grunt.registerTask('default', ['jshint', 'test']);
+	// grunt.registerTask('default', ['jshint', 'test']);
+
+	grunt.registerTask('default', ['jshint', 'clean', 'styleguide']);
 
 };
